bdd083d67232a7775cc69a34e5bcc468dd6d16de
LazyDev3D <leon.wil@hotmail.com>
2023-11-27 15:31:52 +0000

    CameraResets

---
+++ Assets/Scripts/CameraReset.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class CameraReset : MonoBehaviour
+{
+    private Vector3 originalCameraPosition;
+    private Quaternion originalCameraRotation;
+
+    public float resetDuration = 1.0f; // Adjust this value for the reset transition duration
+
+    private bool isResetting = false;
+    private float resetTimer = 0.0f;
+
+    void Start()
+    {
+        // Store the original position and rotation of the camera
+        originalCameraPosition = transform.position;
+        originalCameraRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        // Check for the R key press
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // Start the reset transition
+            StartReset();
+        }
+
+        // Update the reset transition if it's in progress
+        if (isResetting)
+        {
+            UpdateReset();
+        }
+    }
+
+    void StartReset()
+    {
+        isResetting = true;
+        resetTimer = 0.0f;
+    }
+
+    void UpdateReset()
+    {
+        // Increment the timer
+        resetTimer += Time.deltaTime;
+
+        // Calculate the interpolation factor based on the timer and reset duration
+        float t = Mathf.Clamp01(resetTimer / resetDuration);
+
+        // Smoothly interpolate the position and rotation towards their original values
+        transform.position = Vector3.Lerp(transform.position, originalCameraPosition, t);
+        transform.rotation = Quaternion.Slerp(transform.rotation, originalCameraRotation, t);
+
+        // Check if the reset transition is complete
+        if (t >= 1.0f)
+        {
+            isResetting = false;
+        }
+    }
+}
---
+++ Assets/Scripts/CameraZoomReset.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class CameraResetScript : MonoBehaviour
+{
+    private Vector3 originalCameraPosition;
+    private Quaternion originalCameraRotation;
+    private float originalZoomDistance;
+
+    public float resetDuration = 1.0f; // Adjust this value for the reset transition duration
+
+    private bool isResetting = false;
+    private float resetTimer = 0.0f;
+
+    void Start()
+    {
+        // Store the original position, rotation, and zoom distance of the camera
+        originalCameraPosition = transform.position;
+        originalCameraRotation = transform.rotation;
+        originalZoomDistance = transform.localPosition.z;
+    }
+
+    void Update()
+    {
+        // Check for the R key press
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // Start the reset transition
+            StartReset();
+        }
+
+        // Update the reset transition if it's in progress
+        if (isResetting)
+        {
+            UpdateReset();
+        }
+    }
+
+    void StartReset()
+    {
+        isResetting = true;
+        resetTimer = 0.0f;
+    }
+
+    void UpdateReset()
+    {
+        // Increment the timer
+        resetTimer += Time.deltaTime;
+
+        // Calculate the interpolation factor based on the timer and reset duration
+        float t = Mathf.Clamp01(resetTimer / resetDuration);
+
+        // Smoothly interpolate the position, rotation, and zoom towards their original values
+        transform.position = Vector3.Lerp(transform.position, originalCameraPosition, t);
+        transform.rotation = Quaternion.Slerp(transform.rotation, originalCameraRotation, t);
+
+        float newZoomDistance = Mathf.Lerp(transform.localPosition.z, originalZoomDistance, t);
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, newZoomDistance);
+
+        // Check if the reset transition is complete
+        if (t >= 1.0f)
+        {
+            isResetting = false;
+        }
+    }
+}
