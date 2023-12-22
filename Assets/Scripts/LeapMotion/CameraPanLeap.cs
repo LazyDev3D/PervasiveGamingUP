8c921e6e03b533d13496b247bc22cc6cb449055e
LazyDev3D <leon.wil@hotmail.com>
2023-12-22 15:26:11 +0000

    Leap Camera Panning

--- Assets/Scripts/LeapMotion/CameraPanLeap.cs
+++ Assets/Scripts/LeapMotion/CameraPanLeap.cs
@@ -1,50 +1,59 @@
 using UnityEngine;
+using Leap.Unity.Interaction;
 
-public class CameraPanLeap : MonoBehaviour
+public class LeapMotionSliderCameraPan : MonoBehaviour
 {
     public float panSpeed = 0.1f;
+    public InteractionSlider horizontalSlider;
+    public InteractionSlider verticalSlider;
 
     private bool isPanning = false;
-    private Vector3 lastSliderPosition;
+    private float lastHorizontalValue;
+    private float lastVerticalValue;
 
     void Update()
     {
-        // Handle camera panning
+        // Handle camera panning with the InteractionSliders
         HandlePanning();
     }
 
     void HandlePanning()
     {
-        if (Input.GetMouseButtonDown(0)) // Assuming left mouse button for interacting with the slider
+        if (horizontalSlider != null && verticalSlider != null)
         {
-            isPanning = true;
-            lastSliderPosition = Input.mousePosition;
-        }
+            float horizontalValue = horizontalSlider.HorizontalSliderValue;
+            float verticalValue = verticalSlider.VerticalSliderValue;
 
-        if (Input.GetMouseButtonUp(0))
+            if (!isPanning)
+            {
+                isPanning = true;
+                lastHorizontalValue = horizontalValue;
+                lastVerticalValue = verticalValue;
+            }
+
+            // Calculate the differences in slider values
+            float deltaHorizontal = horizontalValue - lastHorizontalValue;
+            float deltaVertical = verticalValue - lastVerticalValue;
+
+            // Pan the camera based on the slider movements
+            PanCamera(deltaHorizontal, deltaVertical);
+
+            // Update the last slider values
+            lastHorizontalValue = horizontalValue;
+            lastVerticalValue = verticalValue;
+        }
+        else
         {
             isPanning = false;
         }
-
-        if (isPanning)
-        {
-            // Calculate the difference in slider position
-            Vector3 deltaSlider = Input.mousePosition - lastSliderPosition;
-
-            // Pan the camera based on the slider movement
-            PanCamera(deltaSlider);
-
-            // Update the last slider position
-            lastSliderPosition = Input.mousePosition;
-        }
     }
 
-    void PanCamera(Vector3 deltaSlider)
+    void PanCamera(float deltaHorizontal, float deltaVertical)
     {
-        // Convert the slider movement to a pan direction in world space
-        Vector3 panDirection = new Vector3(deltaSlider.x, 0, deltaSlider.y) * panSpeed;
+        // Convert the slider movements to pan directions in world space
+        Vector3 panDirection = new Vector3(deltaHorizontal, deltaVertical, 0) * panSpeed;
 
         // Apply the pan direction to the camera's position
-        transform.Translate(panDirection, Space.Self);
+        transform.Translate(panDirection, Space.World);
     }
 }
